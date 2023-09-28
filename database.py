049ed0fde6f4e9f9d86083c3e98f5b13f2af7a37
peauli <preciousajorgba@gmail.com>
2023-09-28 08:17:24 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,1 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./cdt3.db"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
